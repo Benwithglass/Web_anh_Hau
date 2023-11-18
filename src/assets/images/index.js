e75f3779df8743134269b5ccf378758e79e4f973
Ben.with.glasses <130723394+Benwithglass@users.noreply.github.com>
2023-11-18 11:33:39 +0700

    1st commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,89 @@
+import logo from "./orebiLogo.png";
+import logoLight from "./logoLight.png";
+import bannerImgOne from "./banner/bannerImgOne.webp";
+import bannerImgTwo from "./banner/bannerImgTwo.webp";
+import bannerImgThree from "./banner/bannerImgThree.webp";
+import bannerImgFour from "./banner/bannerImgFour.webp";
+import bannerImgFive from "./banner/bannerImgFive.webp";
+import bannerImgSix from "./banner/bannerImgSix.webp";
+import saleImgOne from "./sale/saleImgOne.webp";
+import saleImgTwo from "./sale/saleImgTwo.webp";
+import saleImgThree from "./sale/saleImgThree.webp";
+// ============== Products Start here ====================
+// New Arrivals
+import newArrOne from "./products/newArrival/newArrOne.webp";
+import newArrTwo from "./products/newArrival/newArrTwo.webp";
+import newArrThree from "./products/newArrival/newArrThree.webp";
+import newArrFour from "./products/newArrival/newArrFour.webp";
+import pallet_1 from "./products/newArrival/pallet_1.webp";
+import pallet_2 from "./products/newArrival/pallet_2.webp";
+import pallet_3 from "./products/newArrival/pallet_3.webp";
+import pallet_4 from "./products/newArrival/pallet_4.webp";
+import pallet_5 from "./products/newArrival/pallet_5.webp";
+import pallet_6 from "./products/newArrival/pallet_6.webp";
+import pallet_7 from "./products/newArrival/pallet_7.webp";
+import pallet_8 from "./products/newArrival/pallet_8.webp";
+
+
+// Best Sellers
+import bestSellerOne from "./products/bestSeller/bestSellerOne.webp";
+import bestSellerTwo from "./products/bestSeller/bestSellerTwo.webp";
+import bestSellerThree from "./products/bestSeller/bestSellerThree.webp";
+import bestSellerFour from "./products/bestSeller/bestSellerFour.webp";
+
+// Special Offers
+import spfOne from "./products/specialOffer/spfOne.webp";
+import spfTwo from "./products/specialOffer/spfTwo.webp";
+import spfThree from "./products/specialOffer/spfThree.webp";
+import spfFour from "./products/specialOffer/spfFour.webp";
+
+// Year Product
+import productOfTheYear from "./products/productOfTheYear.webp";
+// ============== Products End here ======================
+import paymentCard from "./payment.png";
+import emptyCart from "../images/emptyCart.png";
+
+export {
+  logo,
+  logoLight,
+  bannerImgOne,
+  bannerImgTwo,
+  bannerImgThree,
+  bannerImgFour,
+  bannerImgFive,
+  bannerImgSix,
+  saleImgOne,
+  saleImgTwo,
+  saleImgThree,
+  // ===================== Products start here ============
+  // New Arrivals
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+  pallet_1,
+  pallet_2,
+  pallet_3,
+  pallet_4,
+  pallet_5,
+  pallet_6,
+  pallet_7,
+  pallet_8,
+  // Best Sellers
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+
+  // Sprcial Offers
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+
+  // Year Product
+  productOfTheYear,
+  // ===================== Products End here ==============
+  paymentCard,
+  emptyCart,
+};
